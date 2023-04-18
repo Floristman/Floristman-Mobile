f124a90e9337d96f72478220cdf2e510a7162853
IlyosErkinovvvv <ilyoserkinovvv@gmail.com>
2023-04-18 12:39:04 +0500

    register in login

--- floristman/lib/main.dart
+++ floristman/lib/main.dart
@@ -1,1 +1,20 @@
-void main(){}
+import 'package:floristman/view/register.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+void main(){
+  runApp(MyWidget());
+} 
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Register(),
+    );
+  }
+}
---
+++ floristman/lib/view/register.dart
@@ -0,0 +1,332 @@
+
+import 'package:floristman/view/log_in.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffC4C4C4),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: const Color(0xffFFF0E4),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(40.0),
+                            bottomRight: Radius.circular(40.0),
+                            topLeft: Radius.circular(40.0),
+                            bottomLeft: Radius.circular(40.0),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.03,
+                            ),
+                            Text(
+                              """Регистрация""",
+                              style: GoogleFonts.montserrat(
+                                textStyle: const TextStyle(
+                                  color: Color(0xff272626),
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 25.0,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.03,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  """Есть аккаунт?""",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width:MediaQuery.of(context).size.width * 0.03,
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => const LogInPage(),
+                                      ),
+                                    );
+                                  },
+                                  child: const Text(
+                                    """Войдите""",
+                                    style: TextStyle(
+                                      color: Color(0xffFD4F79),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.02,
+                            ),
+                            // TEXTFORMFIELD Фамилия: //
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.01,
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.05,
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal:
+                                            MediaQuery.of(context).size.width *
+                                                0.05),
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                        hintText: "Телефон",
+                                        hintStyle: GoogleFonts.lato(
+                                          textStyle: const TextStyle(
+                                            color:
+                                                Color.fromRGBO(0, 0, 0, 0.54),
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 15.0,
+                                          ),
+                                        ),
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        border: const OutlineInputBorder(
+                                          borderSide: BorderSide.none,
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(17.0),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.02,
+                            ),
+                            // TEXTFORMFIELD Имя и отчество: //
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.01,
+                                ),SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.05,
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal:
+                                            MediaQuery.of(context).size.width *
+                                                0.05),
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                        hintText: "Имя",
+                                        hintStyle: GoogleFonts.lato(
+                                          textStyle: const TextStyle(
+                                            color:
+                                                Color.fromRGBO(0, 0, 0, 0.54),
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 15.0,
+                                          ),
+                                        ),
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        border: const OutlineInputBorder(
+                                          borderSide: BorderSide.none,
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(17.0),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.02,
+                            ),
+                            // TEXTFORMFIELD Телефон: //
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.01,
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.05,
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal:
+                                            MediaQuery.of(context).size.width *
+                                                0.05),
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                        hintText: "E-mail",
+                                        hintStyle: GoogleFonts.lato(
+                                          textStyle: const TextStyle(
+                                            color:
+                                                Color.fromRGBO(0, 0, 0, 0.54),
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 15.0,
+                                          ),
+                                        ),
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        border: const OutlineInputBorder(
+                                          borderSide: BorderSide.none,
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(17.0),
+                                          ),
+                                        ),
+                                      ),),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.02,
+                            ),
+                            // TEXTFORMFIELD Email: //
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.01,
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.05,
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal:
+                                            MediaQuery.of(context).size.width *
+                                                0.05),
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                        hintText: "Пароль",
+                                        hintStyle: GoogleFonts.lato(
+                                          textStyle: const TextStyle(
+                                            color:
+                                                Color.fromRGBO(0, 0, 0, 0.54),
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 15.0,
+                                          ),
+                                        ),
+                                        suffixIcon: const Icon(
+                                            Icons.remove_red_eye_outlined),
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        border: const OutlineInputBorder(
+                                          borderSide: BorderSide.none,
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(17.0),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.03,
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(22.0),
+                                      side: const BorderSide(
+                                        color: Color(0xffD7537A),
+                                      ),
+                                    ),
+                                  ),
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xffD7537A),
+                                  ),
+                                ),
+                                onPressed: () {},
+                                child: const Text("""Зарегистрироваться"""),
+                              ),),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.03,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
